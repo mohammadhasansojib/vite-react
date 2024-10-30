42f31e47cbd42cc50a4c5262d1023682cb1fe54a
sajib hasan <sajibhasan800080@gmail.com>
2024-10-30 22:16:05 +0600

    changed the text

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
+import './App.css';
+
 export default function App(){
 
-  return <h1 style={{textAlign: 'center'}}>Hello Sajib</h1>
+  return (
+    <>
+      <h1 className='heading'>HELLO WORLD</h1>
+    </>
+  )
 }
